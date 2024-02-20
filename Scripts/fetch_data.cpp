3ae4122818d2b9c90f8ab24e039016bb30f280cc
teknikspec <teknikspec@gmail.com>
2024-02-20 17:27:43 +0100

    Updated C++ script and bash

--- Scripts/fetch_data.cpp
+++ Scripts/fetch_data.cpp
@@ -3,37 +3,10 @@
 #include<iostream>
 #include<string>
 #include<fstream>
-#include<ctime>
 #include<iomanip>
+#include<chrono>
+#include<cstring>
 //##########################
-
-// Function returns formated file name: YY.MM.DD
-std::string get_file_name(std::tm* time) {
-
-    int year = time->tm_year + 1900;
-    int month = time->tm_mon + 1;
-    int day = time->tm_mday;   
-
-    std::string date_formated = std::to_string(year) + "." 
-                              + std::to_string(month) + "."
-                              + std::to_string(day);
-
-    return date_formated;
-}
-
-// Function returns current time
-std::string get_time(std::tm* time) {
-
-    int hour = time->tm_hour;
-    int minute = time->tm_min;
-    int second = time->tm_sec;
-
-    std::string time_formated = std::to_string(hour) + ":" 
-                              + std::to_string(minute) + ":"
-                              + std::to_string(second) + "::";            
-    
-    return time_formated;
-}
 
 // Execute a command and return result. Can be used for other commands aswell
 std::string executeCommand(const char* cmd) {
@@ -64,12 +37,12 @@ int main()
     const char* serial_port = "/dev/ttyACM0";
     const char* CPU_temp_command = "vcgencmd measure_temp";
     const char* request_commands_command = "wget --quiet -O ~/Desktop/hydroponisk_odling/Scripts/cmd.txt 192.168.1.221:3000/commands";
-    const char* current_data_file = "Status_bars/Host/data.txt";
+    const char* current_data_file = "data.txt";
 
     //Define variables
     LibSerial::SerialStream serial_stream(serial_port);
-    std::string serial_data;
     std::string temperature;
+    std::string serial_data;
     
     // Check if serial port was opened. Make sure serial_port is pointing to the arduino serial port.
     if (!serial_stream.IsOpen())
@@ -89,19 +62,31 @@ int main()
     // Loop through the same program
     while (true)
     {
-        // Check for commands as the main priority
-        executeCommand(request_commands_command);
+        // Get current time for file_name and time
+        auto currentTime = std::chrono::system_clock::now();
+        char file_name_buffer[20];
+        char time_buffer[20];
 
+        auto transformed = currentTime.time_since_epoch().count() / 1000000;
+        auto millis = transformed % 1000;
 
-        // Read a line from the serial port
+        std::time_t tt;
+        tt = std::chrono::system_clock::to_time_t ( currentTime );
+        auto timeinfo = localtime (&tt);
+
+        // Formats the file_name and time
+        strftime (file_name_buffer,20,"%F",timeinfo);
+        strftime (time_buffer,20,"%H.%M.%S",timeinfo);
+        sprintf(time_buffer, "%s:%03d",time_buffer,(int)millis);
+
+        // Get data from arduino
         getline(serial_stream, serial_data);
 
-        // Get current time
-        std::time_t current_time = std::time(nullptr);
-        std::tm* local_time = std::localtime(&current_time);
-
+        // Fetch data from Server
+        //executeCommand(request_commands_command);
+        
         // Save to Data directory, which should be located in Desktop
-        std::string saved_data_file = "../Data/" + get_file_name(local_time) + ".txt";
+        std::string saved_data_file = "../Data/" + std::string(file_name_buffer) + ".txt";
 
         // Open files to save the data
         std::ofstream saved_output_file(saved_data_file, std::ios::app);
@@ -112,7 +97,7 @@ int main()
         temperature = temperature.substr(temperature.find('=') + 1, temperature.find('C') - temperature.find('=') - 2);
 
         if (saved_output_file.is_open()) {
-            saved_output_file << get_time(local_time) << "5|" << temperature << "|" << serial_data;
+            saved_output_file << std::string(time_buffer) << "::5|" << temperature << "|" << serial_data;
             saved_output_file.close();
         }
 
